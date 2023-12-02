9a1af687d7caab302e18c1eeb75098e026a238a7
Roy Roodenburg <royroodtje@gmail.com>
2023-12-02 18:56:28 +0100

    fix for deployment

--- src/app/myData/page.tsx
+++ src/app/myData/page.tsx
@@ -1,1 +1,5 @@
-export const
+import { Typography } from '@mui/material';
+
+export default async function MyData() {
+  return <Typography variant="h1">My Data</Typography>;
+}
--- src/frontend/hooks/useVehicleTrips.ts
+++
@@ -1,8 +0,0 @@
-import { useSWR, SwrOptions } from "./useSWR";
-import { getVehicleTrips } from "../api/trips/getVehicleTrips";
-import { Trip } from "@/backend/repository/trips/findAllTripsByVehicleId";
-
-export const getUseVehicleTripsKey = (vehicleId: string) => `useTrips${vehicleId}`;
-export const useVehicleTrips = (vehicleId: string, options?: SwrOptions<any>) => {
-  return useSWR<Trip[]>(getUseVehicleTripsKey(vehicleId), () => getVehicleTrips(vehicleId), options);
-};
